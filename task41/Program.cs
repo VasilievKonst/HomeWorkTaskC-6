2ea193176d4213e2187ac2acab0e9bc3a0048c2d
KonstantinVasiliev <grafit21@mail.ru>
2022-11-06 00:11:20 +0500

    Save task 41

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] ConvAR(string x)
+{
+    Console.WriteLine(x+":");
+    int[] arra = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    return arra;
+}
+int[] array = ConvAR("Введите числа через пробел");
+
+int len = array.Length;
+int count = 0;
+int index = 0;
+
+while(index < len)
+{
+    if(array[index]>0)
+    {
+        count++;
+    }
+    index++;
+}
+
+Console.WriteLine($"Колличество введеных чисел больше нуля равно: {count}");
+
+
+
